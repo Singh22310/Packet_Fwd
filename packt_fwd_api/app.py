ff7e062edd887cf25fa99a0e255bdf476fff47f2
the-nazim <thenazim0707@gmail.com>
2025-05-19 06:25:58 +0000

    API for checking update bug fixed

--- packt_fwd_api/app.py
+++ packt_fwd_api/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 import werkzeug.exceptions
 from dotenv import load_dotenv
 
-import database
+# import database
 
 load_dotenv()
 
@@ -19,18 +19,21 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app)
-database.init_db()
+# database.init_db()
 
-@app.route('/', methods=['GET'])
+@app.route('/update-checker/', methods=['GET'])
 def update_checker():
     response = requests.get(os.getenv("CHECK_UPDATE_API"))
     data = response.json()
     version_data = data["Update details"]
     logger.info(f"Version data: {version_data}")
-    if database.check_update(version_data["id"]):
-        return jsonify({"message": "Update available", "version": version_data["id"]}), 200
-    else:
-        return jsonify({"message": "No update available", "version": version_data["id"]}), 200
+    return jsonify({"message": "Update details fetched successfully", "version": version_data}), 200
+    # if database.check_update(version_data["id"]):
+        # return jsonify({"message": "Update available", "version": version_data["id"]}), 200
+    # else:
+        # return jsonify({"message": "No update available", "version": version_data["id"]}), 200
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
